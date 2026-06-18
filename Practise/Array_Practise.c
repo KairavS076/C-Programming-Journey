9757bc0a5afb63719c427d12305c5384a4131f7d
KairavS076 <kairavsinghal06@gmail.com>
2026-06-18 16:09:18 +0530

    Add files via upload

---
+++ Practise/Array_Practise.c
@@ -0,0 +1,16 @@
+//Array tut
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+    for(int i=0 ; i<10 ; i++)
+    {
+        scanf("\n %d",&arr[i]);
+    }
+    for(int i=0 ; i<10 ; i++)
+    {
+        printf("\n %d",arr[i]);
+    }
+    return 0;
+}
