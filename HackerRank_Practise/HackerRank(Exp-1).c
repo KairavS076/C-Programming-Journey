7c5aef4176a8048539b8bed92df3122364462b01
KairavS076 <kairavsinghal06@gmail.com>
2026-06-18 14:43:45 +0530

    Add files via upload

---
+++ HackerRank_Practise/HackerRank(Exp-1).c
@@ -0,0 +1,12 @@
+//Introduction HackerRank , Hello , World! Challenge
+
+#include <stdio.h>
+
+int main()
+{
+
+    char s[100]="Welcome to C programming.";
+    printf("Hello, World!");
+    printf("\n%s",s);
+    return 0;
+}
